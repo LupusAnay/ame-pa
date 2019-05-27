dbd4d0addff546782cb47a87513350e2afe3fb73
Ilya Kopeshtianski <lupusanay@gmail.com>
2019-05-27 15:41:05 +0300

    Now using database from heroku

--- index.php
+++ index.php
@@ -5,16 +5,23 @@ require 'vendor/autoload.php';
 
 function app_factory()
 {
-    $app = \Base::instance();
+    $app = Base::instance();
     $app->config('app/config.ini');
     $app->config('app/routes.ini');
 
-    $database = new DB\SQL
-    (
-        $app->get('db_type') . ':host=' . $app->get('db_host') . ';port=' . $app->get('db_port') . ';dbname=' . $app->get('db_name'),
-        $app->get('db_login'),
-        $app->get('db_password')
-    );
+    $raw_database_url = getenv('CLEARDB_DATABASE_URL');
+    if (!$raw_database_url) {
+        $raw_database_url = $app->get('database_url');
+    }
+
+    $database_url = parse_url($raw_database_url);
+    $host = $database_url['host'];
+    $user = $database_url['user'];
+    $password = $database_url['pass'];
+    $port = $database_url['port'];
+    $name = ltrim($database_url["path"],'/');
+
+    $database = new DB\SQL("mysql:host=$host;port=$port;dbname=$name", $user, $password);
 
     $app->set('db', $database);
     return $app;
